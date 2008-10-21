0efa4b7d830db940605e0a362202f215221abe2f
juliogreff <juliogreff@f742f696-32bd-4836-b1b6-acfd33ab05fc>
2008-10-21 19:27:37 +0000

    Fixed issue #10, Helper class created
    git-svn-id: http://spaghettiphp.org/subversion/trunk@16 f742f696-32bd-4836-b1b6-acfd33ab05fc

---
+++ core/helper.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  Put description here.
+ *
+ *  Licensed under The MIT License.
+ *  Redistributions of files must retain the above copyright notice.
+ *  
+ *  @package Spaghetti
+ *  @subpackage Spaghetti.Core.Helper
+ *  @license http://www.opensource.org/licenses/mit-license.php The MIT License
+ * 
+ */
+
+class Helper extends Object {
+    public function output($out = "") {
+        return $out;
+    }
+}
+
+?>
