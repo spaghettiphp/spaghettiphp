e0e8f3186ab576f2bc9ffc94a83e57093f28cd35
rafaelmarin <rafaelmarin@f742f696-32bd-4836-b1b6-acfd33ab05fc>
2009-01-09 11:28:56 +0000

    Corrected indentation for core/folder.php
    git-svn-id: http://spaghettiphp.org/subversion/trunk@100 f742f696-32bd-4836-b1b6-acfd33ab05fc

--- core/folder.php
+++ core/folder.php
@@ -5,68 +5,69 @@ class Folder extends FileSystem {
     private $_root = ROOT;
     
     public function __construct($path = null, $mkdir = false){
-		$this->path = $this->_root;
-		if(!is_null($path)):
-		    $this->cd($path);
-		endif;
-		if($mkdir === true):
-			$this->mkdir();
-		endif;
+	$this->path = $this->_root;
+	if(!is_null($path)):
+	    $this->cd($path);
+	endif;
+	if($mkdir === true):
+		$this->mkdir();
+	endif;
     }
     
     private function cd($path = null){
-		return $this->path = $path;
+	return $this->path = $path;
     }
     
     public function isDir() {
-		if(is_dir($this->path)):
-		    return true;
-		endif;
-		return false;
+	if(is_dir($this->path)):
+	    return true;
+	endif;
+	return false;
     }
     
     public function ls($filter = true, $sort = "asc"){
-		$path = $this->path;
-		$sort = ($sort=="asc")? 0 : 1;
-		if($filter):
-		    $files = array();
-		    foreach(scandir($path, $sort) as $file):
-			if(!preg_match("/^\./", $file)):
-			    $files[] = $file;
-			endif;
-		    endforeach;
-		else:
-		    $files = scandir($path, $sort);
+	$path = $this->path;
+	$sort = ($sort=="asc")? 0 : 1;
+	if($filter):
+	    $files = array();
+	    foreach(scandir($path, $sort) as $file):
+		if(!preg_match("/^\./", $file)):
+		    $files[] = $file;
 		endif;
-		return $files;
+	    endforeach;
+	else:
+	    $files = scandir($path, $sort);
+	endif;
+	return $files;
     }
     
     public function rm() {
-		if(!@rmdir($this->path)):
-		    throw new FolderException("rm() - couldnt remove");
-			return false;
-		endif;
-		return true;
+	if(!@rmdir($this->path)):
+	    throw new FolderException("rm() - couldnt remove");
+		return false;
+	endif;
+	return true;
     }
 
-	public function rmdir(){
-		return $this->rm();
-	}
+    public function rmdir(){
+	return $this->rm();
+    }
     
     public function mkdir($recursive = true) {
-		if(@mkdir($this->path, $recursive)):
-		    return true;
-		else:
-		    throw new FolderException("mkdir()");
-		endif;
+	if(@mkdir($this->path, $recursive)):
+	    return true;
+	else:
+	    throw new FolderException("mkdir()");
+	    return false;
+	endif;
     }
     
     public static function stripSlashes($path) {
-		return trim($path, "/");
+	return trim($path, "/");
     }
     
     public function __toString(){
-		return $this->path;
+	return $this->path;
     }
     
 }
