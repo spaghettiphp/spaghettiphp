771921798f60defca778b39524eac276afacc38a
juliogreff <juliogreff@gmail.com>
2009-03-18 14:54:28 -0300

    Enhanced Helper's documentation

--- core/helper.php
+++ core/helper.php
@@ -1,6 +1,7 @@
 <?php
 /**
- *  Short Description
+ *  Helper é uma classe herdada por todos os helpers do Spaghetti, definindo apenas
+ *  os métodos essenciais para seu funcionamento.
  *
  *  @license   http://www.opensource.org/licenses/mit-license.php The MIT License
  *  @copyright Copyright 2008-2009, Spaghetti* Framework (http://spaghettiphp.org/)
@@ -8,6 +9,12 @@
  */
 
 abstract class Helper extends Object {
+    /**
+     *  Retorna conteúdo para ser usado na saída de informações.
+     *
+     *  @param string $out Informação a ser retornada
+     *  @return string Informação retornada
+     */
     public function output($out = "") {
         return $out;
     }
