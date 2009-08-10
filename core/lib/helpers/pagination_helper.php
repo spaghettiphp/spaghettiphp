384164867d7413aa94e87383a06174f931f3c75b
juliogreff <juliogreff@gmail.com>
2009-08-10 14:04:03 -0300

    added PaginationHelper

---
+++ core/lib/helpers/pagination_helper.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *  Short Description
+ *
+ *  @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+ *  @copyright Copyright 2008-2009, Spaghetti* Framework (http://spaghettiphp.org/)
+ *
+ */
+
+class PaginationHelper extends Helper {
+    public function next() {
+        
+    }
+    public function previous() {
+        
+    }
+    public function first() {
+        
+    }
+    public function last() {
+        
+    }
+    public function numbers() {
+        
+    }
+}
+
+?>
