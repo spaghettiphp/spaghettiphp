85b102b858f12a2cc95c3ceb9b807fafe8c3defe
Victor Feitoza <vfeitoza@virtualword.com.br>
2009-08-17 17:53:37 -0300

    Helper Form finalizado

--- core/lib/helpers/form_helper.php
+++ core/lib/helpers/form_helper.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *  Short Description
+ *  Geração automática do formulário em HTML de acordo com os dados passados.
  *
  *  @license   http://www.opensource.org/licenses/mit-license.php The MIT License
  *  @copyright Copyright 2008-2009, Spaghetti* Framework (http://spaghettiphp.org/)
@@ -8,11 +8,25 @@
  */
 
 class FormHelper extends HtmlHelper {
+    /**
+     *  Retorna um elemento HTML do formulário formatado.
+     * 
+     *  @param string $action Ação atual do modelo
+     *  @param array $options Atributos e opções da tag HTML
+     *  @return string Tag FORM aberto e formatado
+     */
     public function create($action = null, $options = array()) {
         $attr = array_merge(array("method" => "post", "action" => Mapper::url($action)), $options);
         $form = $this->openTag("form", $attr);
         return $this->output($form);
     }
+    /**
+     *  Fecha um elemento HTML do formulário de acordo com os atributos repassados.
+     *
+     *  @param boolean $submit Botão e envio do formulário
+     *  @param array $attr Atributos e opções da tag HTML
+     *  @return string Tag FORM fechada
+     */
     public function close($submit = null, $attr = array()) {
         $form = $this->closeTag("form");
         if($submit != null):
@@ -20,24 +34,79 @@ class FormHelper extends HtmlHelper {
         endif;
         return $this->output($form);
     }
+    /**
+     *  Cria um botão de envio dos dados do formulário.
+     *
+     *  @param string $submit Nome do botão de envio
+     *  @param array $attr Atributos e opções da tag
+     *  @return string Botão de envio do formulário
+     */
     public function submit($submit = "", $attr = array()) {
         return $this->output($this->tag("button", $submit, array_merge(array("type" => "submit"), $attr)));
     }
+    /**
+     *  Cria uma caixa de texto.
+     *
+     *  @param string $name Nome da caixa de texto
+     *  @param string $value Conteudo da caixa de texto
+     *  @param array $attr Atributos da tag
+     *  @return string Caixa de texto do formulário
+     */
     public function text($name = "", $value = "", $attr = array()) {
         return $this->output($this->openTag("input", array_merge(array("name" => $name, "value" => $value, "type" => "text"), $attr), false));
     }
+    /**
+     *  Cria uma caixa de texto multi-linhas.
+     *
+     *  @param string $name Nome da caixa de texto
+     *  @param string $value Conteudo da caixa de texto
+     *  @param array $attr Atributos da tag
+     *  @return string Caixa de texto do formulário
+     */
     public function textarea($name = "", $value = "", $attr = array()) {
         return $this->output($this->tag("textarea", $value, array_merge(array("name" => $name), $attr)));
     }
+    /**
+     *  Cria uma caixa de texto para senhas.
+     * 
+     *  @param string $name Nome da caixa de texto
+     *  @param string $value Conteudo da caixa de texto
+     *  @param array $attr Atributos da tag
+     *  @return string Caixa de texto do formulário
+     */
     public function password($name = "", $value = "", $attr = array()) {
         return $this->output($this->openTag("input", array_merge(array("name" => $name, "value" => $value, "type" => "password"), $attr), false));
     }
+    /**
+     *  Cria uma caixa de texto para enviar arquivos.
+     * 
+     *  @param string $name Nome da caixa de envio de arquivo
+     *  @param string $value Conteudo da caixa de envio de arquivo
+     *  @return string Caixa de envio de arquivo do formulário
+     */
     public function file($name = "", $attr = array()) {
         return $this->output($this->openTag("input", array_merge(array("name" => $name, "type" => "file"), $attr), false));
     }
+    /**
+     *  Cria um campo oculto.
+     * 
+     *  @param string $name Nome do campo oculto
+     *  @param string $value Conteudo do campo oculto
+     *  @param array $attr Atributos da tag
+     *  @return string Campo oculto do formulário
+     */
     public function hidden($name = "", $value = "", $attr = array()) {
         return $this->output($this->openTag("input", array_merge(array("name" => $name, "value" => $value, "type" => "hidden"), $attr), false));
     }
+    /**
+     *  Cria uma caixa de seleção.
+     * 
+     *  @param string $name Nome da caixa de seleção
+     *  @param string $value Conteudo da caixa de seleção
+     *  @param string $selected Opção selecionada por padrão
+     *  @param array $attr Atributos da tag
+     *  @return string Caixa de seleção do formulário
+     */
     public function select($name = "", $values = array(), $selected = "", $attr = array()) {
         $options = "";
         foreach($values as $key => $value):
@@ -49,6 +118,14 @@ class FormHelper extends HtmlHelper {
         endforeach;
         return $this->tag("select", $options, array_merge(array("name" => $name), $attr));
     }
+    /**
+     *  Cria caixa de entrada formatada e com label.
+     * 
+     *  @param string $name Nome do campo de entrada
+     *  @param string $value Conteudo do campo de entrada
+     *  @param array $attr Atributos da tag
+     *  @return string Campo de entrada do formulário
+     */
     public function input($name = "", $value = "", $options = array()) {
         $options = array_merge(array(
             "type" => "text",
@@ -69,6 +146,17 @@ class FormHelper extends HtmlHelper {
         endif;
         return $label != false ? $this->tag("label", "{$label}\n{$input}") : $input;
     }
+    /**
+     *  Cria um conjunto de caixa de seleção para a data.
+     * 
+     *  @param string $name Nome do conjunto de caixas de seleção
+     *  @param int $start_year Ano inicial da seleção
+     *  @param int $end_year Ano final da seleção
+     *  @param int $current_month Mês corrente
+     *  @param int $current_day Dia corrente
+     *  @param int $current_year Ano corrente
+     *  @return string Retorna um conjunto de caixa de seleção
+     */
     public function dateselect($name = "date", $start_year = 1980, $end_year = null, $current_month = null, $current_day = null, $current_year = null) {
         //if default values are not passed, default values should be the current date
         $year_now = (int) date("Y");
