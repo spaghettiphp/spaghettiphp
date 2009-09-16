bc2efad50eeead2b6f3f8ca2de968fca2dc689d2
juliogreff <juliogreff@gmail.com>
2009-09-16 16:56:23 -0300

    added some Validation methods

--- core/validation.php
+++ core/validation.php
@@ -8,7 +8,72 @@
  */
 
 class Validation extends Object {
-    
+    public static function alphaNumeric() {
+        
+    }
+    public static function between() {
+        
+    }
+    public static function blank() {
+        
+    }
+    public static function boolean() {
+        
+    }
+    public static function creditCard() {
+        
+    }
+    public static function comparison() {
+        
+    }
+    public static function date() {
+        
+    }
+    public static function decimal() {
+        
+    }
+    public static function email() {
+        
+    }
+    public static function equal() {
+        
+    }
+    public static function file() {
+        // extension?
+    }
+    public static function ip() {
+        
+    }
+    public static function minLength() {
+        
+    }
+    public static function maxLength() {
+        
+    }
+    public static function money() {
+        
+    }
+    public static function multiple() {
+        
+    }
+    public static function inList() {
+        
+    }
+    public static function numeric() {
+        
+    }
+    public static function notEmpty() {
+        
+    }
+    public static function range() {
+        
+    }
+    public static function time() {
+        
+    }
+    public static function url() {
+        
+    }
 }
 
 ?>
