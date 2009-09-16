3d69116da781b1e00db5a4ee1620a245f8253bce
juliogreff <juliogreff@gmail.com>
2009-09-16 16:43:48 -0300

    added Validation

---
+++ core/validation.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  Short description.
+ *
+ *  @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+ *  @copyright Copyright 2008-2009, Spaghetti* Framework (http://spaghettiphp.org/)
+ *
+ */
+
+class Validation extends Object {
+    
+}
+
+?>
