d5113d12eb9483b514af9013b6fe9dd790737c55
juliogreff <juliogreff@gmail.com>
2009-10-08 10:07:54 -0300

    defining permissions

--- app/controllers/app_controller.php
+++ app/controllers/app_controller.php
@@ -3,7 +3,8 @@
 class AppController extends Controller {
     public $components = array("Auth", "AccessControl");
     public function beforeFilter() {
-        
+        $this->AuthComponent->allow("/");
+        $this->AccessControlComponent->allow("admin", "/home/index");
     }
 }
 
