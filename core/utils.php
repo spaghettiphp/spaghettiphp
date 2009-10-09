596d6f25dc55cad00ac546dd542f8708594ddf61
juliogreff <juliogreff@gmail.com>
2009-10-09 11:39:06 -0300

    removing unused functions from misc.php

--- core/utils.php
+++ core/utils.php
@@ -29,35 +29,7 @@ function dump($data) {
 }
 
 /**
- *  Analisa um texto, retornando o mesmo de acordo com a condição informada.
- * 
- *  @param string $condition Condição a ser verificada
- *  @param string $string Texto a ser analisado
- *  @return string Texto analisado para verdadeiro e nulo para falso
- */
-function if_string($condition, $string) {
-    if($condition && $condition != ""):
-        return $string;
-    endif;
-    return "";
-}
-/**
- *  Verifica todos os argumentos repassados, retornando-os caso
- *  possuam um valor diferente de nulo.
- *
- *  @return string Variável analisada
- */
-function pick() {
-    $args = func_get_args();
-    foreach($args as $arg):
-        if($arg !== null):
-            return $arg;
-        endif;
-    endforeach;
-    return null;
-}
-/**
- *  Limpa o valor de um índice do array repassado.
+ *  Limpa o valor de um índice do array repassado, retornando-o.
  *
  *  @param array $array Array a ser utilizado
  *  @param string $index Índice a ser utilizado
