cc79fedc586d97e85abc7b3941e0a3b9255718d0
Rafael <rafael@MacBook.local>
2009-10-13 20:07:50 -0300

    improved documentation of settings.php

--- app/config/settings.php
+++ app/config/settings.php
@@ -1,20 +1,20 @@
 <?php
 /**
- * Esse é o arquivo das principais configurações do Spaghetti. Através delas,
- * você pode configurar o comportamento do núcleo do Spaghetti.
+ *  Esse é o arquivo das principais configurações do Spaghetti. Através delas,
+ *  você pode configurar o comportamento do núcleo do Spaghetti.
  */
 
 /**
- * defaultExtension define a extensão de arquivo padrão quando nenhuma outra for
- * usada na URL da requisição. Desta maneira, seus arquivos de views deverão se chamar
- * nome_do_arquivo.EXTENSAO.php
+ *  defaultExtension define a extensão de arquivo padrão quando nenhuma outra for
+ *  usada na URL da requisição. Desta maneira, seus arquivos de views deverão se chamar
+ *  nome_do_arquivo.EXTENSAO.php
  */
 Config::write("defaultExtension", "htm");
 
 /**
- * Com o environment, você pode escolher qual ambiente de desenvolvimento está
- * utilizando. É principalmente utilizado na configuração de banco de dados,
- * evitando que você tenha que redefiní-las a cada deploy.
+ *  Com o environment, você pode escolher qual ambiente de desenvolvimento está
+ *  utilizando. É principalmente utilizado na configuração de banco de dados,
+ *  evitando que você tenha que redefiní-las a cada deploy.
  */
 Config::write("environment", "development");
 
@@ -30,7 +30,8 @@ Config::write("debugMode", 1);
 Config::write("appEncoding", "utf-8");
 
 /**
- *  securitySalt é uma string usada na criptografia de dados.
+ *  securitySalt é uma string usada na criptografia de dados. Esta string não
+ *  tem limite de caracteres e aceita qualquer tipo de caracter.
  */
 Config::write("securitySalt", "12345678910111213141516");
 
