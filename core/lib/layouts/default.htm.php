0efe89e29b6d1b7d2f3ae0d26a5ab6726e4534ae
rafaelmarin <rafael@rafaelmarin.net>
2009-10-13 22:47:46 -0300

    removed wrong welcome screen view, changed correct view and layout

--- core/lib/layouts/default.htm.php
+++ core/lib/layouts/default.htm.php
@@ -3,156 +3,197 @@
     <head>
         <meta http-equiv="Content-type" content="text/html; charset=utf-8">
         <title>Spaghetti* Framework</title>
-        <style type="text/css">
-            * {
-                margin:0;
-                padding:0;
-            }
-            html {
-                background: #37312F;
-            }
-            body {
-                background: #23201E;
-                color: #FFFFFF;
-                font: 12px "Helvetica", "Arial", sans-serif;
-                margin: 40px auto;
-                padding: 50px 70px;
-                width: 580px;
-            }
-            
-            hr {
-                border: none;
-                clear: both;
-            }
-            
-            header {
-                border-bottom: 1px solid #444140;
-                display: block;
-                padding-bottom: 40px;
-            }
+		
+		<style type="text/css">
+			* {
+				margin:0;
+				padding:0;
+			}
+			html {
+				background: #37312F;
+			}
+			body {
+				background: #23201E;
+				color: #FFFFFF;
+				font: 12px "Helvetica", "Arial", sans-serif;
+				margin: 40px auto;
+				padding: 50px 70px;
+				width: 580px;
+			}
+			
+			hr {
+				border: none;
+				clear: both;
+			}
+			
+			header {
+				border-bottom: 1px solid #444140;
+				display: block;
+				padding-bottom: 40px;
+			}
 
-                header #logo {
-                    color: #FFFFFF;
-                    font-size: 26px;
-                    font-weight: bold;
-                    letter-spacing: -2px;
-                    text-decoration: none;
-                }
-                    header #logo span {
-                        color: #99CC00;
-                    }
-                    
-                header #info {
-                    -moz-border-radius: 3px;
-                    -webkit-border-radius: 3px;
-                    border: 1px solid #2F2C2A;
-                    color: #99CC00;
-                    display: block;
-                    float: right;
-                    font-size: 11px;
-                    height: 20px;
-                    line-height: 20px;
-                    outline: none;
-                    margin-top: 5px;
-                    text-align: center;
-                    text-decoration: none;
-                    width: 175px;
-                }
-                
-                    header #info:hover {
-                        background: -webkit-gradient(linear, left top, left bottom, color-stop(0.0, #23201E), color-stop(1.0, #111111));
-                        border-color: #444140;
-                    }
-                    
-                    header #info:active,
-                    header #info.on {
-                        background: -webkit-gradient(linear, left top, left bottom, color-stop(0.0, #111111), color-stop(1.0, #23201E));
-                        color: #FFFFFF;
-                    }
-            
-            section#environment {
-                border-bottom: 1px solid #444140;
-                display: none;
-                padding: 20px 0;
-            }
-            
-                section#environment table {
-                    -moz-border-radius: 3px;
-                    -webkit-border-radius: 3px;
-                    border-collapse: collapse;
-                    border: 1px solid #2F2C2A;
-                    width: 100%;
-                }
-                
-                section#environment table th {
-                    background: #282422;
-                    border-bottom: 1px solid #37312F;
-                    padding: 5px 8px;
-                    text-align: left;
-                    width: 180px;
-                }
-                
-                section#environment table td {
-                    border-bottom: 1px solid #2F2C2A;
-                    padding: 5px 8px;
-                    text-align: left;
-                }
-            
-            section#head {
-                border-bottom: 1px solid #444140;
-                display: block;
-                padding: 40px 0;
-            }
-            
-                section#head h1 {
-                    color: #99CC00;
-                    font: lighter 36px "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, arial, sans-serif;
-                    margin-bottom: 20px;
-                }
-                
-                section#head p {
-                    font-size: 15px;
-                    line-height: 22px;
-                }
-            
-            section.features {
-                border-bottom: 1px solid #444140;
-                clear: both;
-                display: block;
-                padding: 40px 0;
-            }
-            
-                section.features div {
-                    float: left;
-                    width: 275px;
-                }
-                
-                    section.features div h2 {
-                        color: #99CC00;
-                        font-size: 16px;
-                        font-weight: normal    ;
-                        margin-bottom: 20px;
-                    }
-                    
-                    section.features div p {
-                        line-height: 18px;
-                    }
-                
-                    section.features div:first-child {
-                        margin-right: 20px;
-                    }
-            
-            footer {
-                display: block;
-                padding-top: 40px;
-            }
-            
-                footer p {
-                    color: #37312F;
-                    font-size: 18px;
-                    text-align: center;
-                }
-        </style>
+				header #logo {
+					color: #FFFFFF;
+					font-size: 26px;
+					font-weight: bold;
+					letter-spacing: -2px;
+					text-decoration: none;
+				}
+					header #logo span {
+						color: #99CC00;
+					}
+					
+				header #info {
+					-moz-border-radius: 3px;
+					-webkit-border-radius: 3px;
+					border: 1px solid #2F2C2A;
+					color: #99CC00;
+					display: block;
+					float: right;
+					font-size: 11px;
+					height: 20px;
+					line-height: 20px;
+					outline: none;
+					margin-top: 5px;
+					text-align: center;
+					text-decoration: none;
+					width: 175px;
+				}
+				
+					header #info:hover {
+						background: -webkit-gradient(linear, left top, left bottom, color-stop(0.0, #23201E), color-stop(1.0, #111111));
+						border-color: #444140;
+					}
+					
+					header #info:active,
+					header #info.on {
+						background: -webkit-gradient(linear, left top, left bottom, color-stop(0.0, #111111), color-stop(1.0, #23201E));
+						color: #FFFFFF;
+					}
+			
+			section#environment {
+				border-bottom: 1px solid #444140;
+				display: none;
+				padding: 20px 0;
+			}
+			
+				section#environment table {
+					-moz-border-radius: 3px;
+					-webkit-border-radius: 3px;
+					border-collapse: collapse;
+					border: 1px solid #2F2C2A;
+					width: 100%;
+				}
+				
+				section#environment table th {
+					background: #282422;
+					border-bottom: 1px solid #37312F;
+					padding: 5px 8px;
+					text-align: left;
+					width: 180px;
+				}
+				
+				section#environment table td {
+					border-bottom: 1px solid #2F2C2A;
+					padding: 5px 8px;
+					text-align: left;
+				}
+			
+			section#head {
+				border-bottom: 1px solid #444140;
+				display: block;
+				padding: 40px 0;
+			}
+			
+				section#head h1 {
+					color: #99CC00;
+					font: lighter 36px "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, arial, sans-serif;
+					margin-bottom: 20px;
+				}
+				
+				section#head p {
+					font-size: 15px;
+					line-height: 22px;
+				}
+				
+			section#quickstart {
+				border-bottom: 1px solid #444140;
+				display: block;
+				padding: 40px 0;
+			}
+			
+				section#quickstart div {
+					float: left;
+					margin-right: 20px;
+					width: 130px;
+				}
+			
+					section#quickstart div:nth-child(4) {
+						margin-right: 0;
+					}
+				
+				section#quickstart div span {
+					color: #99CC00;
+					font: lighter 36px "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, arial, sans-serif;
+					margin-bottom: 20px;
+				}
+				
+				section#quickstart div h3 {
+					font-size: 12px;
+					letter-spacing: -0.5px;
+					margin-bottom: 10px;
+				}
+				
+				section#quickstart div p {
+					color: #6E6766;
+					font-size: 11px;
+				}
+				
+				section#quickstart div:hover p {
+					color: #FFF;
+				}
+			
+			section.features {
+				border-bottom: 1px solid #444140;
+				clear: both;
+				display: block;
+				padding: 40px 0;
+			}
+			
+				section.features div {
+					float: left;
+					width: 275px;
+				}
+				
+					section.features div h2 {
+						color: #99CC00;
+						font-size: 16px;
+						font-weight: normal	;
+						margin-bottom: 20px;
+					}
+					
+					section.features div p {
+						line-height: 18px;
+					}
+				
+					section.features div:first-child {
+						margin-right: 20px;
+					}
+			
+			footer {
+				display: block;
+				padding-top: 40px;
+			}
+			
+				footer p {
+					color: #37312F;
+					font-size: 18px;
+					text-align: center;
+				}
+		</style>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/mootools/1.2.3/mootools-yui-compressed.js"></script>
+		<script type="text/javascript">
+		</script>
     </head>
     
     <body>
--- core/lib/views/home/index.htm.php
+++ core/lib/views/home/index.htm.php
@@ -3,10 +3,33 @@
 	<p>Agora você já pode começar a desenvolver sua aplicação com Spaghetti*. Abaixo você encontra informações iniciais sobre onde obter a documentação, guia do usuário, fóruns de suporte e componentes para estender as funcionalidades da sua aplicação.</p>
 </section>
 
+<section id="quickstart">
+	<div>
+		<span>1</span>
+		<h3>Configure seu arquivo de banco de dados</h3>
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+	</div>
+	<div>
+		<span>2</span>
+		<h3>Configure seu arquivo de banco de dados</h3>
+		<p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+	</div>
+	<div>
+		<span>3</span>
+		<h3>Escreva seu primeiro controller</h3>
+		<p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et</p>
+	</div>
+	<div>
+		<span>4</span>
+		<h3>Crie views e layouts para suas actions</h3>
+		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </p>
+	</div>
+	<hr class="clear" />
+</section>
+
 <section class="features">
 	<div>
 		<h2>Como começar</h2>
-
 		<p>Agora você já pode começar a desenvolver sua aplicação com Spaghetti*. Abaixo você encontra informações iniciais sobre onde documentação, guia do usuário, fóruns de suporte e componentes para estender as funcionalidades.</p>
 	</div>
 	<div>
@@ -14,7 +37,6 @@
 		<p>Agora você já pode começar a desenvolver sua aplicação com Spaghetti*. Abaixo você encontra informações iniciais sobre onde documentação, guia do usuário, fóruns de suporte e componentes para estender as funcionalidades.</p>
 	</div>
 	<hr class="clear" />
-
 </section>
 
 <section class="features">
@@ -23,7 +45,6 @@
 		<p>Agora você já pode começar a desenvolver sua aplicação com Spaghetti*. Abaixo você encontra informações iniciais sobre onde documentação, guia do usuário, fóruns de suporte e componentes para estender as funcionalidades.</p>
 	</div>
 	<div>
-
 		<h2>Componentes</h2>
 		<p>Agora você já pode começar a desenvolver sua aplicação com Spaghetti*. Abaixo você encontra informações iniciais sobre onde documentação, guia do usuário, fóruns de suporte e componentes para estender as funcionalidades.</p>
 	</div>
