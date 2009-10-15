bf83014b25d9daa48d8c48145d68f33b9f6f1433
rafaelmarin <rafael@rafaelmarin.net>
2009-10-14 22:07:45 -0300

    improved error messages and layout

--- core/lib/layouts/default.htm.php
+++ core/lib/layouts/default.htm.php
@@ -203,33 +203,7 @@
         </header>
 
         <section id="environment">
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Versão do Spaghetti*</th>
-                        <td><?php echo SPAGHETTI_VERSION ?></td>
-                    </tr>
-                    <tr>
-                        <th>Ambiente</th>
-                        <td><?php echo Config::read("environment") ?></td>
-                    </tr>
-                    <tr>
-                        <th>Versão do PHP</th>
-                        <td><?php echo phpversion() ?></td>
-                    </tr>
-					<?php if(function_exists("apache_get_version")): ?>
-                    <tr>
-                        <th>Servidor</th>
-                        <td><?php echo apache_get_version() ?></td>
-                    </tr>
-					<?php endif ?>
-                    <tr>
-                        <th>Caminho raiz</th>
-                        <td><?php echo ROOT ?></td>
-                    </tr>
-					
-                </tbody>
-            </table>
+            <?php echo $this->element("environment"); ?>
         </section>
 
         <?php echo $this->contentForLayout ?>
--- core/lib/views/errors/missing_model.htm.php
+++ core/lib/views/errors/missing_model.htm.php
@@ -1,2 +1,2 @@
-<h1>O model <?php echo $details["model"] ?> não foi encontrado.</h1>
+<h1>Model não encontrado.</h1>
 <p>O model não foi encontrado. O arquivo <code>app/models/<?php echo $details["model"] ?>.php</code> realmente existe?</p>
