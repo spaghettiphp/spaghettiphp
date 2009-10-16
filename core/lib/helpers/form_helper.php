6dbd4b9611c5838ef12f77aa07a2d2fe263efd42
juliogreff <juliogreff@gmail.com>
2009-10-16 11:06:59 -0300

    refactored and moved FormHelper::dateselect() to FormHelper::date() (fixes #7)

--- core/lib/helpers/form_helper.php
+++ core/lib/helpers/form_helper.php
@@ -156,6 +156,8 @@ class FormHelper extends HtmlHelper {
         elseif($options["type"] == "radio"):
             $label = false;
             $input = $this->radio($name, $options);
+        elseif($options["type"] == "date"):
+            $input = $this->date($name, $options);
         else:
             if($options["type"] == "hidden"):
                 $div = $label = false;
@@ -179,61 +181,31 @@ class FormHelper extends HtmlHelper {
      *  Cria um conjunto de caixa de seleção para a data.
      * 
      *  @param string $name Nome do conjunto de caixas de seleção
-     *  @param int $start_year Ano inicial da seleção
-     *  @param int $end_year Ano final da seleção
-     *  @param int $current_month Mês corrente
-     *  @param int $current_day Dia corrente
-     *  @param int $current_year Ano corrente
-     *  @return string Retorna um conjunto de caixa de seleção
+     *  @return string Conjunto de caixa de seleção
      */
-    public function dateselect($name = "date", $start_year = 1980, $end_year = null, $current_month = null, $current_day = null, $current_year = null) {
-        //if default values are not passed, default values should be the current date
-        $year_now = (int) date("Y");
-        $day_now = (int) date("d");
-        $month_now = (int) date("m");
-        if(!$end_year) $end_year = $year_now;
-        if(!$current_year) $current_year = $year_now;
-        if(!$current_month) $current_month = $month_now;
-        if(!$current_day) $current_day = $day_now;
-
-        //day select
-        $select_day = '<select name="' . $name . '[d]" id="' . $name . '_day">';
-
-        //day select options
+    public function date($name, $options = array()) {
+        $options = array_merge(array(
+            "startYear" => 1980,
+            "endYear" => date("Y"),
+            "currentDay" => date("d"),
+            "currentMonth" => date("m"),
+            "currentYear" => date("Y")
+        ), $options);
+        $days = $months = $years = array();
         for($i = 1; $i < 32; $i++):
-            $select_day .= '<option value="' . $i . '"';
-            if($i == $current_day) $select_day .= ' selected="selected"';
-            $select_day .= '>' . $i . '</option>';
+            $days[$i] = $i;
         endfor;
-
-        $select_day .= "</select>";
-
-        //month select
-        $select_month = '<select name="' . $name . '[m]" id="' . $name . '_month">';
-
-        //month select options
         for($i = 1; $i < 13; $i++):
-            $select_month .= '<option value="' . $i . '"';
-            if($i == $current_month) $select_month .= ' selected="selected"';
-            $select_month .= '>' . $i . '</option>';
+            $months[$i] = $i;
         endfor;
-
-        $select_month .= "</select>";
-
-        //year select
-        $select_year = '<select name="' . $name . '[y]" id="' . $name . '_year">';
-
-        //year select options
-        for($i = $start_year; $i < $end_year + 1; $i++):
-            $select_year .= '<option value="' . $i . '"';
-            if($i == $current_year) $select_year .= ' selected="selected"';
-            $select_year .= '>' . $i . '</option>';
+        for($i = $options["startYear"]; $i < $options["endYear"] + 1; $i++):
+            $years[$i] = $i;
         endfor;
-
-        $select_year .= "</select>";
-
-        return $select_day . $select_month . $select_year;
-	}
+        $selectDay = $this->select($name . "[d]", array("value" => $options["currentDay"], "options" => $days));
+        $selectMonth = $this->select($name . "[m]", array("value" => $options["currentMonth"], "options" => $months));
+        $selectYear = $this->select($name . "[y]", array("value" => $options["currentYear"], "options" => $years));
+        return $selectDay . $selectMonth . $selectYear;
+    }
 }
 
 ?>
