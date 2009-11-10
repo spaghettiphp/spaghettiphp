18a2810f7ea55ad23ac400fcdc9563372f9157bd
juliogreff <juliogreff@gmail.com>
2009-11-10 09:05:10 -0200

    enhanced DateHelper's documentation

--- core/lib/helpers/date_helper.php
+++ core/lib/helpers/date_helper.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *  Short description.
+ *  DateHelper provê funções de formatação de data.
  *
  *  @license   http://www.opensource.org/licenses/mit-license.php The MIT License
  *  @copyright Copyright 2008-2009, Spaghetti* Framework (http://spaghettiphp.org/)
@@ -8,6 +8,13 @@
  */
 
 class DateHelper extends Helper {
+    /**
+      *  Formata uma data.
+      *
+      *  @param string $format Formato de data
+      *  @param string $date Data compatível com strtotime
+      *  @return string Data formatada
+      */
     public function format($format, $date) {
         $timestamp = strtotime($date);
         return date($format, $timestamp);
