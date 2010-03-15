ecfa3fc912a87ec0c19f955e30255f0980ee053d
juliogreff <juliogreff@gmail.com>
2010-03-15 13:40:58 -0300

    removing debug feature

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -1,8 +1,6 @@
 <?php
 
-$date = microtime(true);
 require dirname(dirname(dirname(__FILE__))) . '/config/bootstrap.php';
 
 $dispatcher = new Dispatcher;
 $dispatcher->dispatch();
-pr(microtime(true) - $date);
