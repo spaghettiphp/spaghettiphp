197707a65103bb31c94c97f22509fd62f3950602
juliogreff <juliogreff@gmail.com>
2010-03-15 11:10:36 -0300

    moving View

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -19,6 +19,8 @@ require 'lib/core/model/Model.php';
 
 require 'lib/core/controller/Controller.php';
 
+require 'lib/core/view/View.php';
+
 require 'lib/core/basics.php';
 require 'lib/core/class_registry.php';
 require 'lib/core/component.php';
@@ -30,7 +32,6 @@ require 'lib/core/security.php';
 require 'lib/core/session.php';
 require 'lib/core/utils.php';
 require 'lib/core/validation.php';
-require 'lib/core/view.php';
 
 require 'config/settings.php';
 require 'config/routes.php';
