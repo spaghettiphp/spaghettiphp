8a23dea46e1ed04075a2ac49717859a344f74dcc
juliogreff <juliogreff@gmail.com>
2010-03-30 14:27:21 -0300

    removed some useless references in ClassRegistry

--- lib/core/class_registry.php
+++ lib/core/class_registry.php
@@ -11,9 +11,9 @@ class ClassRegistry {
         endif;
         return self::$instance;
     }
-    public static function &load($class, $type = 'Model') {
-        $self =& self::instance();
-        if($object =& $self->duplicate($class, $class)):
+    public static function load($class, $type = 'Model') {
+        $self = self::instance();
+        if($object = $self->duplicate($class, $class)):
             return $object;
         elseif(!class_exists($class)):
             if(Loader::exists($type, Inflector::underscore($class))):
@@ -25,29 +25,29 @@ class ClassRegistry {
         endif;
         return ${$class};
     }
-    public static function addObject($key, &$object) {
-        $self =& self::instance();
+    public static function addObject($key, $object) {
+        $self = self::instance();
         if(!array_key_exists($key, $self->objects)):
-            $self->objects[$key] =& $object;
+            $self->objects[$key] = $object;
             return true;
         endif;
         return false;
     }
-    public static function &getObject($key) {
-        $self =& self::instance();
+    public static function getObject($key) {
+        $self = self::instance();
         $return = false;
         if(array_key_exists($key, $self->objects)):
-            $return =& $self->objects[$key];
+            $return = $self->objects[$key];
         endif;
         return $return;
     }
-    public static function &duplicate($key, $class) {
-        $self =& self::instance();
+    public static function duplicate($key, $class) {
+        $self = self::instance();
         $duplicate = false;
         if(array_key_exists($key, $self->objects)):
-            $object =& self::getObject($key);
+            $object = self::getObject($key);
             if($object instanceof $class):
-                $duplicate =& $object;
+                $duplicate = $object;
             endif;
             unset($object);
         endif;
