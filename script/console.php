2e5662e58997068f2dd6db7cfc4de20760223810
juliogreff <juliogreff@gmail.com>
2010-04-20 14:13:42 -0300

    added script/console

---
+++ script/console.php
@@ -0,0 +1,6 @@
+<?php
+
+require dirname(dirname(__FILE__)) . '/config/bootstrap.php';
+define('CONSOLE_ROOT', SPAGHETTI_ROOT . '/lib/shell/console/');
+
+require_once CONSOLE_ROOT . '/Console.php';
