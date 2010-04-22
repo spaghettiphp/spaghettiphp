f30fc519e42f3d8236d0396722f2aaafe8f42289
juliogreff <juliogreff@gmail.com>
2010-04-22 10:37:56 -0300

    refactoring Upload

--- lib/utils/Upload.php
+++ lib/utils/Upload.php
@@ -1,61 +1,47 @@
 <?php
 
+require_once 'lib/core/filesystem/Filesystem.php';
+
 class Upload {
     public $allowedTypes = array();
     public $maxSize = 2;
-    public $path = "/";
+    public $path = '/';
     public $files = array();
     public $errors = array();
     
     public function validates($file = array()) {
-        if(empty($file) && !isset($file["name"])):
-            return $this->error("InvalidParam");
+        if(empty($file) && !isset($file['name'])):
+            return $this->error('InvalidParam');
         endif;
-        if($file["size"] > $this->maxSize * 1024 * 1024):
-            return $this->error("FileSizeExceeded");
+        if($file['size'] > $this->maxSize * 1024 * 1024):
+            return $this->error('FileSizeExceeded');
         endif;
-        if(!empty($this->allowedTypes) && !in_array($this->ext($file["name"]), $this->allowedTypes)):
-            return $this->error("FileTypeNotAllowed");
+        if(!empty($this->allowedTypes) && !in_array(Filesystem::extension($file['name']), $this->allowedTypes)):
+            return $this->error('FileTypeNotAllowed');
         endif;
-        if($uploadError = $this->UploadError($file["error"])):
+        if($uploadError = $this->uploadError($file['error'])):
             return $this->error($uploadError);
         endif;
         return true;
     }
     public function uploadFile($file = array(), $path = null, $name = null) {
         $path = is_null($path) ? $this->path : $path;
-        $name = is_null($name) ? $file["name"] : $name;
+        $name = is_null($name) ? $file['name'] : $name;
         if($this->validates($file)):
-            $path = SPAGHETTI_ROOT . "/public" . $path;
+            $path = SPAGHETTI_ROOT . '/public' . $path;
             if(!is_dir($path)):
                 mkdir($path, 0777, true);
             endif;
-            if(move_uploaded_file($file["tmp_name"], $path . '/' . $name)):
+            if(move_uploaded_file($file['tmp_name'], $path . '/' . $name)):
                 return true;
             else:
-                return $this->error("CantMoveFile");
+                return $this->error('CantMoveFile');
             endif;
         else:
             return false;
         endif;
     }
-    public function delete($filename = "", $path = null) {
-        $path = is_null($path) ? $this->path : $path;
-        $file = SPAGHETTI_ROOT . "/public" . $path . "/" . $filename;
-        if(file_exists($file)):
-            if(unlink($file)):
-                return true;
-            else:
-                return $this->error("CantDeleteFile");
-            endif;
-        else:
-            return $this->error("CantFindFile");
-        endif;
-    }
-    public function ext($filename = "") {
-        return strtolower(trim(substr($filename, strrpos($filename, ".") + 1, strlen($filename))));
-    }
-    public function error($type = "", $details = array()) {
+    public function error($type, $details = array()) {
         $this->errors []= $type;
         return false;
     }
@@ -64,17 +50,23 @@ class Upload {
         return true;
     }
     public function uploadError($error = 0) {
-        $message = false;
         switch($error):
-            case UPLOAD_ERR_OK: break;
-            case UPLOAD_ERR_INI_SIZE: $message = "IniFileSizeExceeded"; break;
-            case UPLOAD_ERR_FORM_SIZE: $message = "FormFileSizeExceeded"; break;
-            case UPLOAD_ERR_PARTIAL: $message = "PartiallyUploaded"; break;
-            case UPLOAD_ERR_NO_FILE: $message = "NoFile"; break;
-            case UPLOAD_ERR_NO_TMP_DIR: $message = "MissingTempDir"; break;
-            case UPLOAD_ERR_CANT_WRITE: $message = "CantWriteFile"; break;
-            default: $message = "UnknownFileError";
+            case UPLOAD_ERR_OK:
+                return false;
+            case UPLOAD_ERR_INI_SIZE:
+                return 'IniFileSizeExceeded';
+            case UPLOAD_ERR_FORM_SIZE:
+                return 'FormFileSizeExceeded';
+            case UPLOAD_ERR_PARTIAL:
+                return 'PartiallyUploaded';
+            case UPLOAD_ERR_NO_FILE:
+                return 'NoFile';
+            case UPLOAD_ERR_NO_TMP_DIR:
+                return 'MissingTempDir';
+            case UPLOAD_ERR_CANT_WRITE:
+                return 'CantWriteFile';
+            default:
+                return 'UnknownFileError';
         endswitch;
-        return $message;
     }
 }
