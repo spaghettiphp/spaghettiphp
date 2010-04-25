09375919e3ff05232a6e8f3978aaabf9df306f85
juliogreff <juliogreff@gmail.com>
2010-04-25 10:46:20 -0300

    refactoring QueryBuilder

--- lib/core/model/QueryBuilder.php
+++ lib/core/model/QueryBuilder.php
@@ -3,6 +3,10 @@
 class QueryBuilder extends Object {
     protected $connection;
     protected $values;
+    protected static $operators = array(
+        '=', '<>', '!=', '<=', '<', '>=', '>', '<=>', 'LIKE', 'REGEXP',
+        '\(\)', 'BETWEEN'
+    );
 
     public function __construct($connection) {
         $this->connection = $connection;
@@ -14,20 +18,52 @@ class QueryBuilder extends Object {
         return $this->connection->$method($params);
     }
     public function extractValues(&$params) {
+        $iterate = array('conditions');
         $values = array();
         
-        foreach($params['conditions'] as $k => $param):
-            if(!is_numeric($k)):
-                $params['conditions'][$k] = $k . ' ?';
-                $values []= $param;
-            endif;
+        foreach($iterate as $key):
+            $result = $this->evaluate($params[$key]);
+            $params[$key] = $result['sql'];
+            $values += $result['values'];
         endforeach;
-        
-        $params['conditions'] = implode(' AND ', array_values($params['conditions']));
         
         return $values;
     }
     public function values() {
         return $this->values;
+    }
+    protected function evaluate($params) {
+        $return = array(
+            'values' => array(),
+            'sql' => array()
+        );
+        
+        foreach($params as $k => $param):
+            if(!is_numeric($k)):
+                list($field, $fn, $operator) = $this->field($k);
+                if(!is_array($param)):
+                    $return['sql'] []= $field . ' '. $operator . ' ?';
+                    $return['values'] []= $param;
+                else:
+                    $repeat = rtrim(str_repeat('?,', count($param)), ',');
+                    $return['sql'] []= $field . ' ' . $fn . '(' . $repeat . ')';
+                    $return['values'] += array_values($param);
+                endif;
+            endif;
+        endforeach;
+        
+        $return['sql'] = implode(' AND ', $return['sql']);
+       
+        return $return;
+    }
+    protected function field($field) {
+        $regex = '/^([\w_]+)\s?(\w+)?(' . join('|', self::$operators) . ')?$/';
+        preg_match($regex, $field, $result);
+        array_shift($result);
+        if(empty($result[2])):
+            $result[2] = '=';
+        endif;
+        
+        return $result;
     }
 }
