620ce5aa5cc0d693fc343e2a98cc53117786a92f
juliogreff <juliogreff@gmail.com>
2010-08-31 08:42:56 -0300

    removed console

--- script/console.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require dirname(dirname(__FILE__)) . '/config/bootstrap.php';
-define('CONSOLE_ROOT', SPAGHETTI_ROOT . '/lib/shell/console/');
-
-require_once CONSOLE_ROOT . '/Console.php';
