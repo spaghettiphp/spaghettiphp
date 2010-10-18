4c11a17956638d6ca94d508a02cc9db55986f934
Claudio Medeiros <klawdyo@gmail.com>
2010-10-17 17:14:09 -0700

    Incluída a possibilidade de personalização dos demarcadores de parâmetros, permitindo o reuso de dessa classe no XgridHelper, já que o padrão de passagem de parâmetros é diferente.

--- lib/core/common/String.php
+++ lib/core/common/String.php
@@ -1,10 +1,13 @@
 <?php
 
 class String {
+    public static $start = ':';
+    public static $end = '';
+
     public static function insert($string, $data) {
         asort($data);
         foreach($data as $key => $value):
-            $regex = '%(:' . $key . ')%';
+            $regex = '%(' . self::$start . $key . self::$end . ')%';
             $string = preg_replace($regex, $value, $string);
         endforeach;
         
