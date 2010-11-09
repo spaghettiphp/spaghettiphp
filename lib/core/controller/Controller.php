afb3f978b18ed137b70055d8a855ec59a94d7e36
juliogreff <juliogreff@gmail.com>
2010-11-09 15:10:31 -0200

    added Controller::__get() and Controller::__set(). yay!

--- lib/core/controller/Controller.php
+++ lib/core/controller/Controller.php
@@ -13,68 +13,97 @@ class Controller {
     protected $params = array();
     protected $uses = null;
     protected $view = array();
+    protected $models = array();
 
     public function __construct() {
-        if(is_null($this->name)):
+        if(is_null($this->name)) {
             $this->name = $this->name();
-        endif;
+        }
 
-        if(is_null($this->uses)):
-            if($this->name == 'App'):
+        if(is_null($this->uses)) {
+            if($this->name == 'App') {
                 $this->uses = array();
-            else:
+            }
+            else {
                 $this->uses = array($this->name);
-            endif;
-        endif;
+            }
+        }
         
         array_map(array($this, 'loadModel'), $this->uses);
         array_map(array($this, 'loadComponent'), $this->components);
         $this->data = array_merge_recursive($_POST, $_FILES);
     }
+
+    public function __set($name, $value) {
+        $this->view[$name] = $value;
+    }
+
+    public function __get($name) {
+        $attrs = array('models', 'view');
+        
+        foreach($attrs as $attr) {
+            if(array_key_exists($name, $this->{$attr})) {
+                return $this->{$attr}[$name];
+            }
+        }
+        
+        throw new RuntimeException(get_class($this) . '->' . $name . ' does not exist.');
+    }
+
     public static function load($name, $instance = false) {
-        if(!class_exists($name) && Filesystem::exists('app/controllers/' . Inflector::underscore($name) . '.php')):
-            require_once 'app/controllers/' . Inflector::underscore($name) . '.php';
-        endif;
-        if(class_exists($name)):
-            if($instance):
+        $filename = 'app/controllers/' . Inflector::underscore($name) . '.php';
+        if(!class_exists($name) && Filesystem::exists($filename)) {
+            require_once $filename;
+        }
+        if(class_exists($name)) {
+            if($instance) {
                 return new $name();
-            else:
+            }
+            else {
                 return true;
-            endif;
-        else:
+            }
+        }
+        else {
             throw new MissingControllerException(array(
                 'controller' => $name
             ));
-        endif;
+        }
     }
+    
     public static function hasViewForAction($request) {
         return Loader::exists('View', $request['controller'] . '/' . $request['action'] . '.' . $request['extension']);
     }
+    
     public function name() {
         $classname = get_class($this);
         $lenght = strpos($classname, 'Controller');
         
         return substr($classname, 0, $lenght);
     }
+    
     public function callAction($request) {
-        if($this->hasAction($request['action']) || Controller::hasViewForAction($request)):
+        if($this->hasAction($request['action']) || Controller::hasViewForAction($request)) {
             return $this->dispatch($request);
-        else:
+        }
+        else {
             throw new MissingActionException(array(
                 'controller' => $request['controller'],
                 'action' => $request['action']
             ));
-        endif;
+        }
     }
+    
     public function hasAction($action) {
         $class = new ReflectionClass(get_class($this));
-        if($class->hasMethod($action)):
+        if($class->hasMethod($action)) {
             $method = $class->getMethod($action);
             return $method->class != 'Controller' && $method->isPublic();
-        else:
+        }
+        else {
             return false;
-        endif;
+        }
     }
+    
     protected function dispatch($request) {
         $this->params = $request;
         $this->componentEvent('initialize');
@@ -82,57 +111,66 @@ class Controller {
         $this->componentEvent('startup');
         $view = View::path($request);
     
-        if($this->hasAction($request['action'])):
+        if($this->hasAction($request['action'])) {
             call_user_func_array(array($this, $request['action']), $request['params']);
             $view = null;
-        endif;
+        }
 
         $output = '';
-        if($this->autoRender):
+        if($this->autoRender) {
             $this->beforeRender();
             $output = $this->render($view);
-        endif;
+        }
 
         $this->componentEvent('shutdown');
         $this->afterFilter();
     
         return $output;
     }
+    
     protected function loadModel($model) {
         $model = Inflector::camelize($model);
-        return $this->{$model} = Model::load($model);
+        return $this->models[$model] = Model::load($model);
     }
+    
     protected function loadComponent($component) {
         $component = Inflector::camelize($component) . 'Component';
         return $this->{$component} = Component::load($component, true);
     }
+    
     protected function componentEvent($event) {
-        foreach($this->components as $component):
+        foreach($this->components as $component) {
             $className = $component . 'Component';
             $this->$className->{$event}($this);
-        endforeach;
+        }
     }
+    
     protected function beforeFilter() { }
+    
     protected function beforeRender() { }
+    
     protected function afterFilter() { }
+    
     public function setAction($action) {
         $args = func_get_args();
         $this->params['action'] = array_shift($args);
         
         return call_user_func_array(array($this, $action), $args);
     }
+    
     public function render($action = null) {
         $view = new View;
         $layout = $this->autoLayout ? $this->layout : false;
         $view->controller = $this;
         $this->autoRender = false;
         
-        if(is_null($action)):
+        if(is_null($action)) {
             $action = Inflector::underscore($this->name) . '/' . $this->params['action'] . '.' . $this->params['extension'];
-        endif;
+        }
 
         return $view->render($action, $this->view, $layout);
     }
+    
     public function redirect($url, $status = null, $exit = true) {
         $this->autoRender = false;
         $codes = array(
@@ -176,48 +214,58 @@ class Controller {
             503 => 'Service Unavailable',
             504 => 'Gateway Time-out'
         );
-        if(!is_null($status) && isset($codes[$status])):
+        
+        if(!is_null($status) && isset($codes[$status])) {
             header('HTTP/1.1 ' . $status . ' ' . $codes[$status]);
-        endif;
+        }
+
         header('Location: ' . Mapper::url($url, true));
         if($exit) $this->stop();
     }
+
     public function set($var, $value = null) {
-        if(is_array($var)):
-            foreach($var as $key => $value):
+        if(is_array($var)) {
+            foreach($var as $key => $value) {
                 $this->set($key, $value);
-            endforeach;
-        else:
+            }
+        }
+        else {
             $this->view[$var] = $value;            
-        endif;
+        }
     }
+
     public function get($var) {
-        if(array_key_exists($var, $this->view)):
+        if(array_key_exists($var, $this->view)) {
             return $this->view[$var];
-        else:
+        }
+        else {
             return null;
-        endif;
+        }
     }
+
     public function param($key, $default = null) {
-        if(array_key_exists($key, $this->params['named'])):
+        if(array_key_exists($key, $this->params['named'])) {
             return $this->params['named'][$key];
-            
-        elseif(in_array($key, array_keys($this->params))):
+        }
+        elseif(in_array($key, array_keys($this->params))) {
             return $this->params[$key];
-            
-        else:
+        }
+        else {
             return $default;
-        endif;
+        }
     }
+
     public function page($param = 'page') {
         return (integer) $this->param($param, 1);
     }
+
     public function isXhr() {
-        if(array_key_exists('HTTP_X_REQUESTED_WITH', $_SERVER)):
+        if(array_key_exists('HTTP_X_REQUESTED_WITH', $_SERVER)) {
             return $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest';
-        endif;
+        }
         return false;
     }
+    
     public function stop() {
         exit(0);
     }
