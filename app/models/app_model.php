d8d3333a174769525bccf43a6301575ca11b393a
juliogreff <juliogreff@gmail.com>
2011-01-12 17:45:48 -0200

    removed comments from AppModel

--- app/models/app_model.php
+++ app/models/app_model.php
@@ -1,16 +1,5 @@
 <?php
-/**
- *  AppModel é o model usado como base para todos os outros models da aplicação.
- *  Como está na biblioteca, é usado apenas quando não houver outro AppModel
- *  definido pelo usuário.
- *
- *  @license   http://www.opensource.org/licenses/mit-license.php The MIT License
- *  @copyright Copyright 2008-2009, Spaghetti* Framework (http://spaghettiphp.org/)
- *
- */
-    
+
 class AppModel extends Model {
     
 }
-    
-?>
