7db7918022c46d2006cc2f3afd0a2714cbf215d1
juliogreff <juliogreff@gmail.com>
2011-03-26 21:36:53 -0300

    Removing things that should be external libraries

--- lib/helpers/DateHelper.php
+++ lib/helpers/DateHelper.php
@@ -6,6 +6,7 @@ class DateHelper extends Helper {
     public function format($date, $format) {
         return Date::format($date, $format);
     }
+
     public function timeAgo($date) {
         return Date::timeAgo($date);
     }
